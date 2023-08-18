a7069ea00d107041b3b84b2787ac8bd290d7748d
ryan zmuda <ryanzmuda@gmail.com>
2023-08-18 15:42:26 -0400

    minesweeper example setup

--- examples/minesweeper/minesweeper.c
+++ examples/minesweeper/minesweeper.c
@@ -1,4 +1,5 @@
 #include "../../build/linux/yoyoengine/lib/engine.h"
+#include "../../build/linux/yoyoengine/lib/graphics.h"
 
 #include <stdbool.h>
 
@@ -10,7 +11,24 @@ int main() {
     };
 
     initEngine(data);
+    
+    createImage(1,.5,.5,1,1,getEngineResourceStatic("enginelogo.png"),true,ALIGN_MID_CENTER);
+
+    bool quit = false;
+    SDL_Event e;
+
+    while(!quit) {
+        while (SDL_PollEvent(&e)) {
+            switch(e.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+
+        // render frame
+        renderAll();
+    }
+
     shutdownEngine();
 }
-
-// see notebook for next steps
