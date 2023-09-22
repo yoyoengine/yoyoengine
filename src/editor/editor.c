01a1ba9a172f6a8f92459c723795cb99e0aa48d4
ryan zmuda <ryanzmuda@gmail.com>
2023-09-21 22:49:54 -0400

    Rudementary launcher impl

--- src/editor/editor.c
+++ src/editor/editor.c
@@ -1,3 +1,4 @@
+ 
 /*
     Use Nuklear to add some editor ui as well as a smaller viewport synced to the current scene event
 
@@ -6,4 +7,19 @@
     - we need a way to put the viewport in a corner or even a seperate window?
 
     do we want this to live in this folder that its in rn? how to seperate the engine from the core? it needs to ship with the core
+
+    Constraints:
+    - editor only supported on linux
 */
+
+// main function accepting one string argument for the path to the project folder
+int main(int argc, char **argv) {
+    // get our path from the command line
+    char *path = argv[1];
+
+    // print the path
+    printf("path: %s\n", path);
+
+    // exit
+    return 0;
+}
