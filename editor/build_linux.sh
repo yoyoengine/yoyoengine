0e5d9a8acb902ff3b14c931065eab046b3d3e0ee
ryan zmuda <ryanzmuda@gmail.com>
2024-07-28 13:55:09 -0400

    fix: first time build no longer requires --reconfigure

--- editor/build_linux.sh
+++ editor/build_linux.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Create a new build directory
-mkdir "./build"
+# Check for the --reconfigure flag
+FORCE_RECONFIGURE=false
+if [ "$1" == "--reconfigure" ]; then
+    FORCE_RECONFIGURE=true
+fi
+
+# Create a new build directory if it doesn't exist
+mkdir -p "./build"
 
 # Navigate into the build directory
 cd "./build"
 
-# only runs cmake if it recieves --reconfigure
-if [ "$1" == "--reconfigure" ]
-then
-    # Run cmake to configure the project
+# Check if CMakeCache.txt exists or if reconfigure is forced
+if [ ! -f "CMakeCache.txt" ] || [ "$FORCE_RECONFIGURE" == true ]; then
     echo "Running cmake..."
     cmake -DCMAKE_BUILD_TYPE=Debug ..
-    if [ $? -eq 0 ]
-    then
+    if [ $? -eq 0 ]; then
         echo "cmake configuration succeeded."
     else
         echo "cmake configuration failed."
@@ -24,8 +27,7 @@ fi
 # Run make to build the project
 echo "Running make..."
 make -j4
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
     echo "make build succeeded."
 else
     echo "make build failed."
