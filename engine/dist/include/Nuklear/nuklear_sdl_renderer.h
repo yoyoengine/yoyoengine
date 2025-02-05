84d9103bb83dc4b5ff8d34643780519b9759b8c4
ryan zmuda <ryanzmuda@gmail.com>
2025-02-05 17:54:11 -0500

    fix: got verticies to display right, but clipping is weird

--- engine/dist/include/Nuklear/nuklear_sdl_renderer.h
+++ engine/dist/include/Nuklear/nuklear_sdl_renderer.h
@@ -25,14 +25,8 @@ NK_API int                  nk_sdl_handle_event(SDL_Event *evt);
 NK_API void                 nk_sdl_render(enum nk_anti_aliasing);
 NK_API void                 nk_sdl_shutdown(void);
 
-#if SDL_COMPILEDVERSION < SDL_VERSIONNUM(2, 0, 22)
-/* Metal API does not support cliprects with negative coordinates or large
- * dimensions. The issue is fixed in SDL2 with version 2.0.22 but until
- * that version is released, the NK_SDL_CLAMP_CLIP_RECT flag can be used to
- * ensure the cliprect is itself clipped to the viewport.
- * See discussion at https://discourse.libsdl.org/t/rendergeometryraw-producing-different-results-in-metal-vs-opengl/34953
- */
-#define NK_SDL_CLAMP_CLIP_RECT
+#if SDL_MAJOR_VERSION < 3
+#error "nuklear_sdl3_renderer requires at least SDL 3.0.0"
 #endif
 
 #endif /* NK_SDL_RENDERER_H_ */
@@ -61,7 +55,7 @@ struct nk_sdl_device {
 struct nk_sdl_vertex {
     float position[2];
     float uv[2];
-    float col[4]; // TODO: MIGRATION: IS THIS RIGHT??
+    float col[4];
 };
 
 static struct nk_sdl {
@@ -97,9 +91,6 @@ nk_sdl_render(enum nk_anti_aliasing AA)
 
     {
         SDL_Rect saved_clip;
-#ifdef NK_SDL_CLAMP_CLIP_RECT
-        SDL_Rect viewport;
-#endif
         bool clipping_enabled;
         int vs = sizeof(struct nk_sdl_vertex);
         size_t vp = offsetof(struct nk_sdl_vertex, position);
@@ -114,9 +105,9 @@ nk_sdl_render(enum nk_anti_aliasing AA)
         /* fill converting configuration */
         struct nk_convert_config config;
         static const struct nk_draw_vertex_layout_element vertex_layout[] = {
-            {NK_VERTEX_POSITION, NK_FORMAT_FLOAT, NK_OFFSETOF(struct nk_sdl_vertex, position)},
-            {NK_VERTEX_TEXCOORD, NK_FORMAT_FLOAT, NK_OFFSETOF(struct nk_sdl_vertex, uv)},
-            {NK_VERTEX_COLOR, NK_FORMAT_R8G8B8A8, NK_OFFSETOF(struct nk_sdl_vertex, col)},
+            {NK_VERTEX_POSITION,    NK_FORMAT_FLOAT,                NK_OFFSETOF(struct nk_sdl_vertex, position)},
+            {NK_VERTEX_TEXCOORD,    NK_FORMAT_FLOAT,                NK_OFFSETOF(struct nk_sdl_vertex, uv)},
+            {NK_VERTEX_COLOR,       NK_FORMAT_R32G32B32A32_FLOAT,   NK_OFFSETOF(struct nk_sdl_vertex, col)},
             {NK_VERTEX_LAYOUT_END}
         };
         NK_MEMSET(&config, 0, sizeof(config));
@@ -141,36 +132,17 @@ nk_sdl_render(enum nk_anti_aliasing AA)
 
         clipping_enabled = SDL_RenderClipEnabled(sdl.renderer);
         SDL_GetRenderClipRect(sdl.renderer, &saved_clip);
-#ifdef NK_SDL_CLAMP_CLIP_RECT
-        SDL_GetRenderViewport(sdl.renderer, &viewport);
-#endif
 
         nk_draw_foreach(cmd, &sdl.ctx, &dev->cmds)
         {
             if (!cmd->elem_count) continue;
 
             {
-                SDL_Rect r;
-                r.x = cmd->clip_rect.x;
-                r.y = cmd->clip_rect.y;
-                r.w = cmd->clip_rect.w;
-                r.h = cmd->clip_rect.h;
-#ifdef NK_SDL_CLAMP_CLIP_RECT
-                if (r.x < 0) {
-                    r.w += r.x;
-                    r.x = 0;
-                }
-                if (r.y < 0) {
-                    r.h += r.y;
-                    r.y = 0;
-                }
-                if (r.h > viewport.h) {
-                    r.h = viewport.h;
-                }
-                if (r.w > viewport.w) {
-                    r.w = viewport.w;
-                }
-#endif
+                SDL_Rect r = {0, 0, 1920, 1080};
+                // r.x = cmd->clip_rect.x;
+                // r.y = cmd->clip_rect.y;
+                // r.w = cmd->clip_rect.w;
+                // r.h = cmd->clip_rect.h;
                 SDL_SetRenderClipRect(sdl.renderer, &r);
             }
 
@@ -180,10 +152,12 @@ nk_sdl_render(enum nk_anti_aliasing AA)
                 SDL_RenderGeometryRaw(sdl.renderer,
                         (SDL_Texture *)cmd->texture.ptr,
                         (const float*)((const nk_byte*)vertices + vp), vs,
-                        (const SDL_FColor*)((const float*)vertices + vc), vs, // TODO: MIGRATION: IS THIS RIGHT??
+                        (const SDL_FColor*)((const nk_byte*)vertices + vc), vs,
                         (const float*)((const nk_byte*)vertices + vt), vs,
                         (vbuf.needed / vs),
                         (void *) offset, cmd->elem_count, 2);
+
+                printf("clip rect: %f %f %f %f\n", cmd->clip_rect.x, cmd->clip_rect.y, cmd->clip_rect.w, cmd->clip_rect.h);
 
                 offset += cmd->elem_count;
             }
@@ -226,25 +200,6 @@ nk_sdl_clipboard_copy(nk_handle usr, const char *text, int len)
 NK_API struct nk_context*
 nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer)
 {
-#ifndef NK_SDL_CLAMP_CLIP_RECT
-    SDL_RendererInfo info;
-    SDL_version runtimeVer;
-
-    /* warn for cases where NK_SDL_CLAMP_CLIP_RECT should have been set but isn't */
-    SDL_GetRendererInfo(renderer, &info);
-    SDL_GetVersion(&runtimeVer);
-    if (strncmp("metal", info.name, 5) == 0 &&
-        SDL_VERSIONNUM(runtimeVer.major, runtimeVer.minor, runtimeVer.patch) < SDL_VERSIONNUM(2, 0, 22))
-    {
-        SDL_LogWarn(
-            SDL_LOG_CATEGORY_APPLICATION,
-            "renderer is using Metal API but runtime SDL version %d.%d.%d is older than compiled version %d.%d.%d, "
-            "which may cause issues with rendering",
-            runtimeVer.major, runtimeVer.minor, runtimeVer.patch,
-            SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_PATCHLEVEL
-        );
-    }
-#endif
     sdl.win = win;
     sdl.renderer = renderer;
     nk_init_default(&sdl.ctx, 0);
